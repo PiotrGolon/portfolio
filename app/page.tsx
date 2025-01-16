e83b814991a4c7eb5553aa3eefa57602b2346be9
Piotr Golon <piotrek98golon@o2.pl>
2025-01-16 23:10:34 +0100

    2:hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Hero from "@/components/hero";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full text-white">
-        {/* <FloatingNav navItems={navItems} />
+      <div className="max-w-7xl w-full">
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
-        <Grid />
+        {/*<Grid />
         <RecentProject />
         <Clients />
         <Experience />
